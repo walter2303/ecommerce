15ead9c46ed463d8501572f5707d0a4a8074d2a3
Walter <walter.mudesto@hotmail.com>
2020-03-13 14:08:22 -0300

    Admin - Categorias

--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -6,47 +6,41 @@ class Model {
 
 	private $values = [];
 
-	public function setData($data)
-	{
-
-		foreach ($data as $key => $value)
-		{
-
-			$this->{"set".$key}($value);
-
-		}
-
-	}
-
 	public function __call($name, $args)
 	{
 
 		$method = substr($name, 0, 3);
 		$fieldName = substr($name, 3, strlen($name));
 
-		if (in_array($fieldName, $this->fields))
+		switch ($method)
 		{
-			
-			switch ($method)
-			{
 
-				case "get":
-					return $this->values[$fieldName];
-				break;
+			case "get":
+				return (isset($this->values[$fieldName])) ? $this->values[$fieldName] : NULL;
+			break;
 
-				case "set":
-					$this->values[$fieldName] = $args[0];
-				break;
-
-			}
+			case "set":
+				$this->values[$fieldName] = $args[0];
+			break;
 
 		}
-	
+
+	}
+
+	public function setData($data = array())
+	{
+
+		foreach ($data as $key => $value) {
+			
+			$this->{"set".$key}($value);
+
+		}
+
 	}
 
 	public function getValues()
 	{
-		
+
 		return $this->values;
 
 	}
